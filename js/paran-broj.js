a27b732a6af7453f0a764dcf97450690bbc347bf
Filip <filip.ratkovic.fr@gmail.com>
2021-12-30 00:46:19 +0100

    paran broj

---
+++ js/paran-broj.js
@@ -0,0 +1,11 @@
+a = window.prompt("Unesite broj");
+
+if (a % 2 === 0) {
+    console.log("Broj je paran")
+} else {
+    console.log("broj je neparan")
+}
+
+if (a*2===0) {
+    console.log("Broj je 0")
+} 
--- js/vezba2.js
+++ js/vezba2.js
@@ -1,14 +1,3 @@
-// a = window.prompt("Unesite broj");
-
-// if (a % 2 === 0) {
-//     console.log("Broj je paran")
-// } else {
-//     console.log("broj je neparan")
-// }
-
-// if (a*2===0) {
-//     console.log("Broj je 0")
-// } 
 
 
 b = parseInt(prompt("Unesite 1. broj"));
@@ -29,63 +18,63 @@ if (d>b && d>c) {
     console.log(d)
 }
 
-// a=window.prompt ("Unesite broj")
+// // a=window.prompt ("Unesite broj")
 
-// switch(a) {
-//     case "1":
-//     console.log ("January")
-//     break;
-//     case "2":
-//     console.log ("February")
-//     break;
-//     case "3":
-//     console.log ("March")
-//     break;
-//     case "4":
-//     console.log ("April")
-//     break;
-//     case "5":
-//     console.log ("May")
-//     break;
-//     case "6":
-//     console.log ("Jun")
-//     break;
-//     case "7":
-//     console.log ("July")
-//     break;
-//     case "8":
-//     console.log ("Avgust")
-//     break;
-//     case "9":
-//     console.log ("September")
-//     break;
-//     case "10":
-//     console.log ("October")
-//     break;
-//     case "11":
-//     console.log ("November")
-//     break;
-//     case "12":
-//     console.log ("December")
-//     break;
-//     default:
-//         console.log ("Pogresan unos");
+// // switch(a) {
+// //     case "1":
+// //     console.log ("January")
+// //     break;
+// //     case "2":
+// //     console.log ("February")
+// //     break;
+// //     case "3":
+// //     console.log ("March")
+// //     break;
+// //     case "4":
+// //     console.log ("April")
+// //     break;
+// //     case "5":
+// //     console.log ("May")
+// //     break;
+// //     case "6":
+// //     console.log ("Jun")
+// //     break;
+// //     case "7":
+// //     console.log ("July")
+// //     break;
+// //     case "8":
+// //     console.log ("Avgust")
+// //     break;
+// //     case "9":
+// //     console.log ("September")
+// //     break;
+// //     case "10":
+// //     console.log ("October")
+// //     break;
+// //     case "11":
+// //     console.log ("November")
+// //     break;
+// //     case "12":
+// //     console.log ("December")
+// //     break;
+// //     default:
+// //         console.log ("Pogresan unos");
 
-// }
+// // }
 
-// a = window.prompt("Unesite broj");
+// // a = window.prompt("Unesite broj");
 
-// if (a<31 && a>0) 
-// {
-// console.log ("Low")
-// }
+// // if (a<31 && a>0) 
+// // {
+// // console.log ("Low")
+// // }
 
-// if (a<71 && a>30) 
-// {
-// console.log ("Medium")
-// }
+// // if (a<71 && a>30) 
+// // {
+// // console.log ("Medium")
+// // }
 
-// if (a<101 && a>70) 
-// {
-// console.log ("High")
-// }
+// // if (a<101 && a>70) 
+// // {
+// // console.log ("High")
+// // }
