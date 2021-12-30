b747932fe61da6252bac69846ce56357380362dc
Filip <filip.ratkovic.fr@gmail.com>
2021-12-30 22:30:14 +0100

    Vezba2

---
+++ js-vezba/vezbaJS.js
@@ -0,0 +1,11 @@
+
+function display () {
+    document.getElementById('display').style.display='block';
+   
+}
+function size () {
+    document.getElementById('size').style.fontSize='35px';
+}
+
+
+
