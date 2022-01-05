074268729ff32656dc2ce98342c03513b8658078
Filip <filip.ratkovic.fr@gmail.com>
2022-01-05 14:11:32 +0100

    Vezba-cas

--- js-vezba/for.js
+++ js-vezba/for.js
@@ -1,25 +1,67 @@
-for (let i=1;i<16;i++) {
+// for (let i=1;i<16;i++) {
     
-    if(i % 2) {
-        console.log(i,"je neparan broj")
-    }
-    else {
-        console.log(i,"je paran broj")
-    }
-}
+//     if(i % 2 === 0) {
+//         console.log(i,"je paran broj")
+//     }
+//     else {
+//         console.log(i,"je neparan broj")
+//     }
+// }
 
 
-a=Number ( prompt('uneste broj'))
-b=Number ( prompt('uneste broj'))
-c=Number ( prompt('uneste broj'))
-d=Number ( prompt('uneste broj'))
+// a=Number ( prompt('uneste broj'))
+// b=Number ( prompt('uneste broj'))
+// c=Number ( prompt('uneste broj'))
+// d=Number ( prompt('uneste broj'))
 
-console.log ("Prosek za uneta 4 broja je",(a+b+c+d)/4)
+// // console.log ("Prosek za uneta 4 broja je",(a+b+c+d)/4)
 
 
+// zbir =0
+// for (let i=0 ; i<1000; i++) {
+//     if(i%3===0 && i%5===0) {
+//         zbir+=i
+//         console.log(zbir)
+//     }
+// }
 
-for (let i=0 ; i<1000; i++) {
-    if(i%3===0 && i%5===0) {
-        console.log(i)
-    }
+// for (let i=0; i<50; i++) {
+// if(i%3===0 && i%5===0){
+//     console.log("Fizzbazz")
+// }
+
+// else if (i%3===0 ) {
+//     console.log ("Fizz")
+//     }
+
+//     else if (i%5===0 ) {
+//         console.log ("buzz")
+//         }
+
+//     else {
+//         console.log (i)
+//     }
+// }
+
+// a=Number( prompt("uneti broj"))
+// fakt=0
+
+// for ( let i=1; i<=a; i++) {
+//     fakt=fakt*1
+
+// }
+// console.log (fakt)
+
+
+br1=1
+br2=1
+
+console.log(br1)
+console.log(br2)
+for (i=1; i<8; i++) {
+sum=br1+br2 
+br1=br2
+br2=sum
+console.log(sum)
+
 }
