a63079e8f19fcd6284336f6f1f6cc206b6f06d2d
Filip <filip.ratkovic.fr@gmail.com>
2022-01-12 22:59:54 +0100

    Domaci, zadatak 1 i 2

--- js-vezba/vezbaif.js
+++ js-vezba/vezbaif.js
@@ -13,28 +13,79 @@
 // }
 // document.getElementById("datum").innerHTML=vreme ;
 
-ucenici= [];
-ucenik = Number (prompt("unesite broj ucenika"))
+// ucenici= [];
+// ucenik = Number (prompt("unesite broj ucenika"))
 
-for (let i = 0; i < ucenik; i++) {
-    visuce = Number (prompt("unesite visinu ucenika"))
-ucenici.push(visuce)
+// for (let i = 0; i < ucenik; i++) {
+//     visuce = Number (prompt("unesite visinu ucenika"))
+// ucenici.push(visuce)
     
+// }
+
+// min=0;
+// max=0;
+
+// if (visuce>max) {
+//     max=visuce
+// }
+// if (visuce<max) 
+// {
+//     min=visuce
+// }
+
+// max = Math.max (...ucenici);
+// min = Math.min (...ucenici);
+// console.log("Visina ucenika je",ucenici)
+// console.log ( "Najnizi ucenik visok je ", min, "Najvisi ucenik visok je ", max) 
+
+// ZADATAK 1 
+
+// niz = [2,3,5,8,9,11,55,78,100,20]
+// niza=0
+// for (i=1 ; i<10 ; i++) 
+// {
+//     niza= ( niz[niz.length -i])
+//     console.log(niza*3)
+
+   
+// }
+
+// ZADATAK 1 A ////
+
+// niz = []
+// niza=0
+
+// for (i=0 ; i<10 ; i++) {
+//     broj=Number(prompt("Unesite broj"))
+//     niz.push(broj)
+
+// }
+
+// for (i=0 ; i<10 ; i++) 
+// {
+//     niza= ( niz[(niz.length-10) +i])
+// console.log(niza*3)
+
+   
+// }
+
+
+// ZADATAK 2 
+
+niz = []
+niza=0
+
+for (i=0 ; i<10 ; i++) {
+    broj=Number(prompt("Unesite broj"))
+    niz.push(broj)
+
 }
 
-min=0;
-max=0;
-
-if (visuce>max) {
-    max=visuce
-}
-if (visuce<max) 
+for (i=0 ; i<10 ; i++) 
 {
-    min=visuce
+    niza= ( niz[(niz.length-10) +i])
+
+    if (niza%2===0)
+    console.log (niza)
+
 }
-
-max = Math.max (...ucenici);
-min = Math.min (...ucenici);
-console.log("Visina ucenika je",ucenici)
-console.log ( "Najnizi ucenik visok je ", min, "Najvisi ucenik visok je ", max) 
-
