b5916fca0c27f0b827fedb02f9af54296e241684
Filip <filip.ratkovic.fr@gmail.com>
2022-01-22 00:52:16 +0100

    switch

---
+++ js-vezba/switch_vezbanje.js
@@ -0,0 +1,63 @@
+// switch (new Date().getDay()) {
+//     case 1:
+//         day = "ponedeljak"
+//         break;
+
+//     case 2:
+//         day = "utorak"
+//         break;
+
+//     case 3:
+//         day = "sreda"
+//         break;
+
+//     case 4:
+//         day = "cetvrtak"
+//         break;
+
+//     case 5:
+//         day = "petak"
+//         break;
+
+//     case 6:
+//         day = "subota"
+//         break;
+
+//     case 7:
+//         day = "nedalja"
+//         break;
+
+// }
+
+// document.getElementById("week").innerHTML = "Danas je " + day + ".";
+
+// year = Number(prompt("Unesite godinu"))
+// month = Number(prompt("Unesite mesec"))
+// let brojDana;
+// switch (month) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
+//         brojDana = 31
+//         break;
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//         brojDana = 30
+//         break;
+// case 2:
+//     if ((year % 4 ==0 && !(year % 100 ==0))|| year % 400 == 0) {
+//         brojDana=29
+//     } else {
+//         brojDana=28
+//     }
+//     break;
+// } 
+
+//     console.log(brojDana)
+
