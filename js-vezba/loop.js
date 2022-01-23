2bd417e52654aab5797e95817cc57170d8e9a3a9
Filip <filip.ratkovic.fr@gmail.com>
2022-01-23 23:54:44 +0100

    suma brojeva deljivih sa 3 i 5 do 1000

--- js-vezba/loop.js
+++ js-vezba/loop.js
@@ -9,19 +9,27 @@
 //     if (i%2 == 0) {
 //         console.log(i, " je paran broj");
 //     } else {console.log(i," je neparan broj");}
-    
 // }
 
-for (var i = 1; i < 10; i++) {
+// for (var i = 1; i < 10; i++) {
 
-    for(var j = 0; j < 10; j++) {
-        for(var k = 0; k < 10; k++) {
-ukupno=(i**3 + j**3 + k**3)
-broj=(i*100 + j*10 + k)
-if(ukupno===broj) {
-    console.log(broj);
-}
-    
-        }
-    }
-}
+//     for(var j = 0; j < 10; j++) {
+//         for(var k = 0; k < 10; k++) {
+// ukupno=(i**3 + j**3 + k**3)
+// broj=(i*100 + j*10 + k)
+// if(ukupno===broj) {
+//     console.log(broj);
+// }
+
+//         }
+//     }
+// }
+
+var sum = 0;
+
+for (i=0;i<1000;i++) {
+
+    if(i%3===0 || i%5===0) {
+        sum+=i
+    } 
+}console.log(sum); 
