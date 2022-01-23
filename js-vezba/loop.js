bec96a8e52efe73ea4d49c74f3744b4ae53ee4f6
Filip <filip.ratkovic.fr@gmail.com>
2022-01-23 23:56:20 +0100

    Brojevi deljivi i sa 3 i sa 5

--- js-vezba/loop.js
+++ js-vezba/loop.js
@@ -25,11 +25,20 @@
 //     }
 // }
 
-var sum = 0;
+// var sum = 0;
+
+// for (i=0;i<1000;i++) {
+
+//     if(i%3===0 || i%5===0) {
+//         sum+=i
+//     } 
+// }console.log(sum); 
+
+ 
 
 for (i=0;i<1000;i++) {
 
-    if(i%3===0 || i%5===0) {
-        sum+=i
+    if(i%3===0 && i%5===0) {
+        console.log(i); 
     } 
-}console.log(sum); 
+}
