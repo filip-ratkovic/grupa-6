7fc59ea95a4939dc92363a35c0a81cdb3fa92a11
Filip <filip.ratkovic.fr@gmail.com>
2022-01-24 22:35:33 +0100

    Biggest and lowest number of array

--- js-vezba/array.js
+++ js-vezba/array.js
@@ -1,7 +1,10 @@
 arr=[ 1,5,9,22,453,12,55];
 
-arr.sort(function(a,b) {return a - b})
+// arr.sort(function(a,b) {return a - b})
 
 
-console.log("Najmanji broj je ",arr[0])
-console.log("Najveci broj je ", arr[arr.length-1]);
+// console.log("Najmanji broj je ",arr[0])
+// console.log("Najveci broj je ", arr[arr.length-1]);
+
+console.log(Math.max(...arr));
+console.log(Math.min(...arr));
