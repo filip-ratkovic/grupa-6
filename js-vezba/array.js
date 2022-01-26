c340bf1f0a4e9a48fd59b6ffcf64f0e598b7fb36
Filip <filip.ratkovic.fr@gmail.com>
2022-01-26 22:56:28 +0100

    sum of squares of a numeric vector

--- js-vezba/array.js
+++ js-vezba/array.js
@@ -9,17 +9,23 @@
 // console.log(Math.max(...arr));
 // console.log(Math.min(...arr));
 
-var a = [
-    [1, 2, 1, 24],
-    [8, 11, 9, 4],
-    [7, 0, 7, 27],
-    [7, 4, 28, 14],
-    [3, 10, 26, 7]];
+// var a = [
+//     [1, 2, 1, 24],
+//     [8, 11, 9, 4],
+//     [7, 0, 7, 27],
+//     [7, 4, 28, 14],
+//     [3, 10, 26, 7]];
 
-    for(var i =0; i <a.length; i++) {
-console.log("row", i,);
-        for (var j = 0; j <a[i].length; j++) {
-            console.log( a[i][j]);
+//     for(var i =0; i <a.length; i++) {
+// console.log("row", i,);
+//         for (var j = 0; j <a[i].length; j++) {
+//             console.log( a[i][j]);
 
-        }
-    }
+//         }
+//     }
+
+a=[0,1,2,3,4];
+let sum = 0;
+for(i=a.length-1; i>=0; i--) {
+sum+=(a[i])**2
+} console.log(sum); 
