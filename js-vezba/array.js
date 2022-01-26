c9ae9c3bf79e81b3b79fccf6bad193faaa7b0592
Filip <filip.ratkovic.fr@gmail.com>
2022-01-26 22:43:50 +0100

    vezba arrays

--- js-vezba/array.js
+++ js-vezba/array.js
@@ -1,4 +1,4 @@
-arr=[ 1,5,9,22,453,12,55];
+// arr = [1, 5, 9, 22, 453, 12, 55];
 
 // arr.sort(function(a,b) {return a - b})
 
@@ -8,3 +8,18 @@ arr=[ 1,5,9,22,453,12,55];
 
 // console.log(Math.max(...arr));
 // console.log(Math.min(...arr));
+
+var a = [
+    [1, 2, 1, 24],
+    [8, 11, 9, 4],
+    [7, 0, 7, 27],
+    [7, 4, 28, 14],
+    [3, 10, 26, 7]];
+
+    for(var i =0; i <a.length; i++) {
+console.log("row", i,);
+        for (var j = 0; j <a[i].length; j++) {
+            console.log( a[i][j]);
+
+        }
+    }
