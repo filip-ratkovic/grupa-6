8440bb608971b94bcefc262b7100fa94f27eb8d7
Filip <filip.ratkovic.fr@gmail.com>
2022-01-27 16:57:22 +0100

    vezba array

--- js-vezba/array.js
+++ js-vezba/array.js
@@ -125,3 +125,12 @@
 // }else {
 //     console.log(b);
 // }
+
+var array1 = [1, 2, 3];
+var array2 = [2, 30, 1];
+
+array3=array1.concat(array2);
+
+array4=[... new Set(array3)];
+
+console.log(array4);
