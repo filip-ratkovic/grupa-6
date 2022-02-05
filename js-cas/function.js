7c4d39634efff582d652d8c7ab7c6ec5a2510433
Filip <filip.ratkovic.fr@gmail.com>
2022-02-05 19:00:32 +0100

    cas,zadatak openOrSenior

--- js-cas/function.js
+++ js-cas/function.js
@@ -379,8 +379,19 @@
 
 
 
+function openOrSenior(data) {
+    for (i = 0; i < data.length; i++) {
 
+        if (data[i][0] >= 55 && data[i][1] > 7) {
+            data[i] = "Senior";
 
+        } else {
+            data[i] = "Open"
+        }
+        
+    }return data
+
+}
 
 
 
