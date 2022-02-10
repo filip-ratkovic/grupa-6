df8fd3310a175c6a38cb67fb4f3aadfe7b556c5c
Filip <filip.ratkovic.fr@gmail.com>
2022-02-11 00:51:12 +0100

    Vezba condition ?

--- js-vezba/vezbaJS.js
+++ js-vezba/vezbaJS.js
@@ -1,7 +1,7 @@
 
 // function display () {
 //     document.getElementById('display').style.display='block';
-   
+
 // }
 // function size () {
 //     document.getElementById('size').style.fontSize='35px';
@@ -9,12 +9,41 @@
 
 
 
-var a=26;
+// var a=26;
 
-age = (a>18) ? "Punoletan" : "Maloletan";
-console.log(age);
+// age = (a>18) ? "Punoletan" : "Maloletan";
+// console.log(age);
+
+// function broj(a) {
+// return a<10 ? "Jednocifren broj"
+// :       a>9&&a<100 ? "dvocifren"
+// :       a>99&&a<1000 ? "Trocifren" 
+// : "vise od 4 cifre"
+// }
+
+// console.log(broj(420));
 
 
+
+
+// function ocena(a) {
+//     return a = 1 ? "Nedovoljan"
+//         : a = 2 ? "Dovoljan"
+//         : a = 3 ? "Dobar"
+//         : a = 4 ? "Vrlodobar"
+//         : a = 5 ? "Odlican"
+//         : "Uneliste neispravni broj";
+// }
+
+// console.log(ocena(3));
+
+
+// let age=18;
+// let message;
+
+// message = age >=18 ? "Sme da vozi" : "Ne sme da vozi"
+
+// console.log(message);
 
 
 
