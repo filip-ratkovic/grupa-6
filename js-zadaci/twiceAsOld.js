f6c71adfa02e4f5b3d1e1d5e45b3b9f9dd16b729
Filip <filip.ratkovic.fr@gmail.com>
2022-02-20 22:21:35 +0100

    Zadatak twiceAsOld

---
+++ js-zadaci/twiceAsOld.js
@@ -0,0 +1,11 @@
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let sum=(dadYearsOld - sonYearsOld * 2)
+    today =2022;
+   godina=today+sum;
+    return godina
+}
+
+
+
+console.log(twiceAsOld(54, 27));
