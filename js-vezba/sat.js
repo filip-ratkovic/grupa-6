f887620ec68fe716ef1ad772a6b00a7c10f93522
Filip <filip.ratkovic.fr@gmail.com>
2022-03-06 00:15:49 +0100

    Vezba sat

--- js-vezba/sat.js
+++ js-vezba/sat.js
@@ -1,42 +1,36 @@
 function showTime() {
 
-    var date = new Date ();
-    var h = date.getHours ();
-    var m = date.getMinutes ();
-    var s = date.getSeconds ();
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
 
-    if (s<10) {
-        s= "0" + s
-    }
+    if (s < 10) {
+        s = "0" + s
+    };
 
-    if (m<10) {
-        m= "0" + m
-    }
+    if (m < 10) {
+        m = "0" + m
+    };
 
-    if (h<10) {
-        h= "0" + h
-    }
-var time = h + ":" + m + ":" + s ;
-
-document.getElementById("sat").innerText = time; 
-document.getElementById("sat").innerContent = time;
-
-setTimeout (showTime,1000);
+    if (h < 10) {
+        h = "0" + h
+    };
+    var time = h + ":" + m + ":" + s;
+    document.getElementById("sat").innerText = time;
+    setTimeout(showTime, 1000);
 }
 
-showTime ();
+showTime();
 
+function getDate() {
+    let date = new Date();
+    let y = date.getFullYear();
+    let m = date.getMonth();
+    let day = date.getDate();
 
-function getDate(){
-let date= new Date ();
-y=date.getFullYear();
-m=date.getMonth();
-day=getDay()
-let datum=day+". "+m+" " + y + "."
-
-document.getElementById("datum").innerText=datum;
-document.getElementById("datum").innerContent=datum;
+    let datum = day + "." + m + "." + y + ".";
+console.log(datum);
+    document.getElementById("datum").innerText = datum;
 }
-
-
-getDate ();
+getDate();
