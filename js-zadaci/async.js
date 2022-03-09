efa39a797a348553485bda20f83f2cdf69b8ec57
Filip <filip.ratkovic.fr@gmail.com>
2022-03-09 14:56:24 +0100

    async fetch zadatak

---
+++ js-zadaci/async.js
@@ -0,0 +1,24 @@
+// async function catBreeds(){
+
+// try{let breeds = await fetch("https://catfact.ninja/breeds")
+// let breedsJson = await breeds.json()
+// let catfact=[];
+// cat=breedsJson.data;
+//  cats = cat.map((el)=>{
+//     return el.breed
+// })
+
+// console.log(cats)} 
+
+// catch {
+//     console.log("error");
+// }
+// }
+
+
+// // catBreeds()
+
+
+
+
+
