e2cfa705aeeff05860d641caf32c7327a2be9d25
Filip <filip.ratkovic.fr@gmail.com>
2022-03-10 00:25:57 +0100

    Async zadatak guessNumber

--- js-zadaci/async.js
+++ js-zadaci/async.js
@@ -1,4 +1,4 @@
-// 1. ZADATAK PRVI
+// 1. ZADATAK//////////////////////////////////////
 // async function catBreeds(){
 
 // try{let breeds = await fetch("https://catfact.ninja/breeds")
@@ -31,68 +31,71 @@
 // console.log(arr);
 
 
-// 2. ZADATAK 2. ////////////////// 
+// 2. ZADATAK//////////////////////////////////////
 
 
-const enterNumber = () => {
-    return new Promise((resolve, reject) => {
-        const userNumber = Number(prompt("Unesite broj od 1 do 10"))
-        const randomNumber =Math.floor( Math.random() * 10 + 1)
-        
-
-        if (isNaN(userNumber)) {
-            reject(new Error("Wrong input type"))
-        }
-   
-        if (userNumber === randomNumber) {
-            resolve({
-                points: 2,
-                randomNumber,
-            })
-        } else if (userNumber === randomNumber + 1 || userNumber === randomNumber - 1) {
-            resolve({
-                points: 1,
-                randomNumber,
-            })
-        } else {
-            resolve({
-                points: 0,
-                randomNumber,
-            })
-        }
-    });
-};
+// const enterNumber = () => {
+//     return new Promise((resolve, reject) => {
+//         const userNumber = Number(prompt("Unesite broj od 1 do 10"))
+//         const randomNumber =Math.floor( Math.random() * 10 + 1)
 
 
+//         if (isNaN(userNumber) || userNumber === 0 || userNumber >10) {
+//             reject(new Error("Wrong input type,try again"))
+//         }
 
-const continueGame = () => {
-    return new Promise((resolve) => {
-        if (window.confirm("Do you want to continue?")) {
-            resolve(true);
-        } else {
-            resolve(false);
-        }
-    });
-};
+//         if (userNumber === randomNumber) {
+//             resolve({
+//                 points: 2,
+//                 randomNumber,
+//             })
+//         } else if (userNumber === randomNumber + 1 || userNumber === randomNumber - 1) {
+//             resolve({
+//                 points: 1,
+//                 randomNumber,
+//             })
+//         }
+//          else {
+//             resolve({
+//                 points: 0,
+//                 randomNumber,
+//             })
+//         }
+//     });
+// };
 
 
-const handleGuest = () => {
-    enterNumber()
-        .then((result) => {
-            alert(`Random number is ${result.randomNumber}, you got ${result.points} points`)
-            
-            continueGame()
-                .then((result) => {
-                    if (result) {
-                        handleGuest()
-                    } else {
-                        alert(`End game`)
-                    }
-                })
-                
-        })
-        
-        .catch((error) => alert(error));
-}
 
-handleGuest()
+// const continueGame = () => {
+//     return new Promise((resolve) => {
+//         if (window.confirm("Do you want to continue?")) {
+//             resolve(true);
+//         } else {
+//             resolve(false);
+//         }
+//     });
+// };
+
+
+// const handleGuess = async () => {
+//     try {
+//         result = await enterNumber()
+//         alert(`Random number is ${result.randomNumber}, you got ${result.points} points`)
+
+//         isContinuing = await continueGame()
+//         if (isContinuing) {
+//             handleGuess()
+//         } else {
+//             alert(`End game`)
+//         }
+//     }
+//     catch (error) {
+//         alert(error);
+//     }
+// }
+
+// handleGuess()
+
+
+
+// 3. ZADATAK//////////////////////////////////////
