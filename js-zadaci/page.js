c5120c92834f7e7df2a318aa1e67962418d3490c
Filip <filip.ratkovic.fr@gmail.com>
2022-03-15 23:39:02 +0100

    Zadatak

--- js-zadaci/page.js
+++ js-zadaci/page.js
@@ -1,9 +1,43 @@
+function stil(vals) {
 
+    var div2 = document.createElement("div")
+    div2.style.width = "200px";
+    div2.style.height = "100px";
+    style = document.createElement("style")
+    div2.appendChild(style)
+    div2.style.backgroundColor = "darkseagreen";
+    div2.style.display = "inline-block"
+    div2.style.flexWrap = "wrap"
+    div2.style.margin = "10px"
+    div2.style.padding = "10px"
+    div2.style.borderRadius = "4px"
+    div2.style.verticalAlign = "middle"
+    div2.style.justifyContent = "space-around"
+    div2.innerHTML = vals
 
-async function factHandler() {
-    let fact=await fetch("https://catfact.ninja/facts")
-    let facts=await fact.json();
-    console.log(fact);
+    return div2
+}
+divUp = document.getElementById("up");
+
+async function text() {
+    list = await fetch("https://catfact.ninja/facts")
+    list = await list.json();
+    // document.getElementById("text-here").innerHTML=list.data[0].fact;
+    for (let i = 0; i < list.data.length; i++) {
+        divUp.appendChild(stil(list.data[i].fact))
+    }
 }
 
-factHandler();
+// text()
+// let currentPage = 1;
+// let start = document.getElementById("start")
+// text=document.getElementById("text")
+// text=window.open("https://catfact.ninja/facts?page=1");
+
+// start.onclick = text
+
+
+// next=document.getElement("page-btn next-page")
+// next.click(function() {
+// currentPage++
+// });
