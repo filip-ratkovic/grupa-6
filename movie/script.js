235da93646487c6989ae94f7ff926f0948709d75
Filip <filip.ratkovic.fr@gmail.com>
2022-04-09 01:56:21 +0200

    Movies reviews

--- movie/script.js
+++ movie/script.js
@@ -3,19 +3,25 @@ const URL =
 const imgURL = "https://image.tmdb.org/t/p/w1280";
 const searchURL =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const pageURL="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page="
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const footer = document.getElementById("footer");
 
 getUrl(URL);
 
-async function getUrl() {
-    res = await fetch(URL);
+async function getUrl(url) {
+    res = await fetch(url);
     finalRes = await res.json();
-    console.log(res);
+    console.log(finalRes);
 getMovies(finalRes.results)
 }
+
+
 
 let getMovies = (movies) => {
 main.innerHTML = ""
@@ -56,15 +62,44 @@ function getClassByRate(vote) {
         return "red";
     }
 }
+next.addEventListener("click",(el)=>{
+    el.preventDefault();
+  page=finalRes.page;
+  page2=page+1;
+    getUrl(pageURL+page2)
+    window.scroll({
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth' 
+       });
+})
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+prev.addEventListener("click",(el)=>{
+    el.preventDefault();
+  page=finalRes.page;
+  if(page<=1) {
+
+  } else {
+    page2=page-1;
+    getUrl(pageURL+page2)
+    window.scroll({
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth' 
+       });
+  }
+  
+})
+
+form.addEventListener("submit", (el) => {
+    el.preventDefault();
 
     const searchValues = search.value;
 
     if(searchValues) {
         getUrl(searchURL + searchValues);
         search.value = "";
+        footer.style.display="none";
     }
         
 
