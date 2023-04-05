203c16ad5d26aafebe74436a6c63dfa3344af156
Filip <filip.ratkovic.fr@gmail.com>
2023-04-05 23:28:33 +0200

    Integers

---
+++ algoritmi/integers.js
@@ -0,0 +1,9 @@
+a=[1,1,2,2,4,4,5,5];
+
+pickingNumbers = (a) => {
+let sortArr = a.sort(function(a,b) { return (a-b)});
+ 
+
+}
+
+console.log(pickingNumbers(a));
--- algoritmi/linearSearch.js
+++ algoritmi/linearSearch.js
@@ -1,9 +1,34 @@
-arr= ["a" , "b" , "c" , "d"]
-const linearSearch = (list, item) => {
-    for(const [index, element] of list.entries()) {
-        if(element === item) {
-                return `Item ${item} is on index ${index}`
-        }
-    }
+// arr= ["a" , "b" , "c" , "d"]
+// const linearSearch = (list, item) => {
+//     for(const [index, element] of list.entries()) {
+//         if(element === item) {
+//                 return `Item ${item} is on index ${index}`
+//         }
+//     }
+// }
+// console.log(linearSearch(arr, "d"));
+
+
+
+if (mainPrefix !== 'zs' && testPrefix !== 'ztest') {
+  const version = words[0]
+  if (version !== 0) {
+    console.log('Unsupported version')
+    return false
+  }
 }
-console.log(linearSearch(arr, "d"));
+
+if (mainPrefix === 'zs' && testPrefix === 'ztest') {
+  const data = encode(words)
+  if (data.length !== 43) {	
+    console.log(`Invalid length: ${data.length}`)	
+    return false	
+  }
+}
+else {
+  const data = encode(words.splice(1))
+  if (data.length !== 20 && data.length !== 32) {	
+    console.log(`Invalid length: ${data.length}`)	
+    return false	
+  }
+}
